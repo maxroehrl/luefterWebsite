380c95bc1225f8d9378e6bc0db79e7f3a6b627d9
Max Röhrl <max.roehrl11@gmail.com>
2024-03-02 17:42:40 +0100

    Update docs

---
+++ docs/assets/vario-vent-CGshEi6n.js
@@ -0,0 +1,1 @@
+import{C as t,e as a}from"./style-BJLJVUVf.js";function n(){import.meta.url,import("_").catch(()=>1),async function*(){}().next()}t.bind("[data-fancybox]",{l10n:a});export{n as __vite_legacy_guard};
