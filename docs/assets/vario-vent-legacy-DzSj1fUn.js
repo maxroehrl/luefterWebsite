8b600ba772b0a0c1bd4d06b6ba77c09e56ba29c4
Max Röhrl <max.roehrl11@gmail.com>
2024-03-02 18:30:37 +0100

    Update docs

--- docs/assets/vario-vent-legacy-DzSj1fUn.js
+++
@@ -1,1 +0,0 @@
-System.register(["./style-legacy-BJDXK12Y.js"],(function(e,t){"use strict";var n,s;return{setters:[e=>{n=e.C,s=e.e}],execute:function(){n.bind("[data-fancybox]",{l10n:s})}}}));
---
+++ docs/assets/vario-vent-legacy-u_QRTWPy.js
@@ -0,0 +1,1 @@
+System.register(["./style-legacy-CKxLa1mm.js"],(function(e,t){"use strict";var n,s;return{setters:[e=>{n=e.C,s=e.e}],execute:function(){n.bind("[data-fancybox]",{l10n:s})}}}));
